e892cd7389a040da7a052a3f637433592d4e8a9a
Manish Purohit ♥ <manishkumar9866@gmail.com>
2019-10-23 02:25:26 +0530

    Update urls.py

--- todolist/urls.py
+++ todolist/urls.py
@@ -17,7 +17,6 @@ from django.conf.urls import url,include
 from django.contrib import admin
 
 urlpatterns = [
-    #url(r'^$', include('todos.urls')),
     url(r'^todos/', include('todoapp.urls')),
     url(r'^admin/', admin.site.urls),
 ]
